bb1fadf05463d6e99df47dd2b23a1f43babbfeac
codermuss <codermuss@gmail.com>
2024-10-13 22:06:46 +0300

    ✨ add theme service logic

--- lib/services/app/theme_service.dart
+++ lib/services/app/theme_service.dart
@@ -1,1 +1,24 @@
-class ThemeService {}
+import 'package:stacked/stacked.dart';
+
+import '../../ui/themes/theme_modes.dart';
+
+class ThemeService with ListenableServiceMixin {
+  /// MARK: - [Properties]
+  ThemeMode _themeMode = ThemeMode.light;
+
+  /// MARK: - [Getters & Setters]
+
+  ThemeMode get themeMode => _themeMode;
+  bool get isDarkMode => _themeMode == ThemeMode.dark;
+
+  /// MARK: - [Methods]
+
+  ThemeService() {
+    listenToReactiveValues([_themeMode]);
+  }
+
+  void toggleTheme() {
+    _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+}
