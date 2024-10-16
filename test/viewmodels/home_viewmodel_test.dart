681bce7de049c66aaddebdcc989c3a7dc3ef8aef
codermuss <codermuss@gmail.com>
2024-10-16 18:11:17 +0300

    ✨ edit tests

--- test/viewmodels/home_viewmodel_test.dart
+++ test/viewmodels/home_viewmodel_test.dart
@@ -1,38 +1,13 @@
-import 'package:blogify/app/app.bottomsheets.dart';
 import 'package:blogify/app/app.locator.dart';
-import 'package:blogify/ui/common/app_strings.dart';
-import 'package:blogify/ui/views/home/home_viewmodel.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 
 import '../helpers/test_helpers.dart';
 
 void main() {
-  HomeViewModel getModel() => HomeViewModel();
-
   group('HomeViewmodelTest -', () {
     setUp(() => registerServices());
     tearDown(() => locator.reset());
 
-    group('incrementCounter -', () {
-      test('When called once should return  Counter is: 1', () {
-        final model = getModel();
-        expect(model.counterLabel, 'Counter is: 1');
-      });
-    });
-
-    group('showBottomSheet -', () {
-      test('When called, should show custom bottom sheet using notice variant', () {
-        final bottomSheetService = getAndRegisterBottomSheetService();
-
-        final model = getModel();
-        model.showBottomSheet();
-        verify(bottomSheetService.showCustomSheet(
-          variant: BottomSheetType.notice,
-          title: ksHomeBottomSheetTitle,
-          description: ksHomeBottomSheetDescription,
-        ));
-      });
-    });
+    group('showBottomSheet -', () {});
   });
 }
