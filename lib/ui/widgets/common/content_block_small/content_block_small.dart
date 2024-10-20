dd7ddbcabb5415d7a3a3a079f27ef053d64380f7
codermuss <codermuss@gmail.com>
2024-10-20 10:03:39 +0300

    ✨feat: add content block small widget

---
+++ lib/ui/widgets/common/content_block_small/content_block_small.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ContentBlockSmall extends StatelessWidget {
+  const ContentBlockSmall({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
