d7f8640a6d3f9e1baaaae9bf05f50cbf3c22ce6f
codermuss <codermuss@gmail.com>
2024-10-21 01:14:41 +0300

    chore: add onboarding view

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -6,6 +6,7 @@ import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:blogify/services/app/theme_service.dart';
 import 'package:blogify/ui/views/sign_up/sign_up_view.dart';
+import 'package:blogify/ui/views/onboarding/onboarding_view.dart';
 // @stacked-import
 
 @StackedApp(
@@ -13,6 +14,7 @@ import 'package:blogify/ui/views/sign_up/sign_up_view.dart';
     MaterialRoute(page: HomeView),
     MaterialRoute(page: StartupView),
     MaterialRoute(page: SignUpView),
+    MaterialRoute(page: OnboardingView),
 // @stacked-route
   ],
   dependencies: [
