e6d41861b8bc96d4ac78881ce67488453d2669d4
codermuss <codermuss@gmail.com>
2024-11-05 22:10:26 +0300

    feat: add github workflow

--- test/viewmodels/sign_in_viewmodel_test.dart
+++ test/viewmodels/sign_in_viewmodel_test.dart
@@ -1,11 +1,15 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:blogify/app/app.locator.dart';
+import 'package:blogify/ui/views/sign_in/sign_in_viewmodel.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 import '../helpers/test_helpers.dart';
 
 void main() {
-  group('SignInViewModel Tests -', () {
+  group('SignInViewModel', () {
     setUp(() => registerServices());
     tearDown(() => locator.reset());
+    test('can be instantiated', () {
+      expect(SignInViewModel(), isNotNull);
+    });
   });
 }
