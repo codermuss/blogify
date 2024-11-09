52df34bd048a366941ea31fc901de80c5378f2a6
codermuss <codermuss@gmail.com>
2024-11-09 15:34:14 +0300

    feat: add onboarding tests

--- test/services/api/onboarding_api_service_test.dart
+++ test/services/api/onboarding_api_service_test.dart
@@ -1,5 +1,8 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:blogify/app/app.locator.dart';
+import 'package:blogify/models/base/base_response.dart';
+import 'package:blogify/models/response/onboarding/onboarding.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 import '../../helpers/test_helpers.dart';
 
@@ -7,5 +10,12 @@ void main() {
   group('OnboardingApiServiceTest -', () {
     setUp(() => registerServices());
     tearDown(() => locator.reset());
+
+    test('get onboarding data success', () async {
+      final onboardingApiService = getAndRegisterOnboardingApiService();
+      when(onboardingApiService.getOnboarding()).thenAnswer((_) async => BaseResponse.success(List<Onboarding>.empty(), null));
+      await onboardingApiService.getOnboarding();
+      verify(onboardingApiService.getOnboarding()).called(1);
+    });
   });
 }
